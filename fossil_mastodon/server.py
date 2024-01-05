50306b0f861ea8707ea4a12b1de8a11485481df7
Alena Chao <alenachao@berkeley.edu>
2024-01-04 18:12:56 -0800

    fixed boost button endpoint

--- fossil_mastodon/server.py
+++ fossil_mastodon/server.py
@@ -160,8 +160,8 @@ async def toots_debug(id: int):
 async def toots_boost(id: int):
     toot = core.Toot.get_by_id(id)
     if toot is not None:
-        from fossil_mastodon.config import ACCESS_TOKEN, MASTO_BASE, headers
-        url = f'{MASTO_BASE}/api/v1/statuses'
+        from fossil_mastodon.config import MASTO_BASE, headers
+        url = f'{MASTO_BASE}/api/v1/statuses/:id/reblog'
         data = {
             'status': toot.content
         }
